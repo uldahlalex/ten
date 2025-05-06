fb759e97bc953a0622568cde97016da628255cc4
Alex Uldahl Pedersen <alex@uldahl.dk>
2025-05-06 11:27:53 +0200

    fix base urls

--- client/src/apiControllerClients.ts
+++ client/src/apiControllerClients.ts
@@ -1,9 +1,9 @@
 import {AuthClient, TicktickTaskClient} from "./generated-client.ts";
 
-const baseUrl = import.meta.env.VITE_API_BASE_URL
+const baseUrlProduction = "https://compulsory25.fly.dev";
+const baseUrlDevelopment = "http://localhost:8080";
 const prod = import.meta.env.PROD
 
- export const authClient = new AuthClient(prod ? "https://" + baseUrl : "http://" + baseUrl);
+ export const authClient = new AuthClient(prod ? baseUrlProduction :  baseUrlDevelopment);
 
-export const taskClient = new TicktickTaskClient(prod ? "https://" + baseUrl : "http://" + baseUrl);
-
+export const taskClient = new TicktickTaskClient(prod ? baseUrlProduction :  baseUrlDevelopment);

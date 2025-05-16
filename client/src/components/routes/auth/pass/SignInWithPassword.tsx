82589389727aba8f362cb0aa7451a229bbf6c108
Alex Uldahl Pedersen <alex@uldahl.dk>
2025-05-16 11:07:06 +0200

    register for production testing

--- client/src/components/routes/auth/pass/SignInWithPassword.tsx
+++ client/src/components/routes/auth/pass/SignInWithPassword.tsx
@@ -2,23 +2,91 @@ import {authClient} from "../../../../apiControllerClients.ts";
 import toast from "react-hot-toast";
 import {useAtom} from "jotai";
 import {JwtAtom} from "../../../../atoms/atoms.ts";
-import {AuthRequestDto} from "../../../../generated-client.ts";
+import {AuthRequestDto, IAuthRequestDto} from "../../../../generated-client.ts";
 import {useNavigate} from "react-router-dom";
 import {TaskListRoute} from "../../../ApplicationRoutes.tsx";
+import {useState} from "react";
 
 export default function SignInWithPassword() {
 
     const [, setJwt] = useAtom(JwtAtom);
+    const [registerForm, setRegisterForm] = useState<IAuthRequestDto>({
+        email: '',
+        password: '',
+    })
+    const [passwordRepeat, setPasswordRepeat] = useState<string>('');
     const navigate = useNavigate();
 
     return (<div className="flex flex-col items-center justify-center h-screen">
-        <button className="btn btn-primary" onClick={() =>
-            authClient.login(new AuthRequestDto({email: "test@user.dk", password: "abc"})).then(r => {
+        <h1 className="text-3xl font-bold mb-4">Register with password</h1>
+ 
+        <form className="space-y-4" onSubmit={(e) => {
+            e.preventDefault();
+            if (registerForm.password !== passwordRepeat) {
+                toast.error("Passwords do not match");
+                return;
+            }
+            authClient.register(new AuthRequestDto(registerForm)).then(r => {
                 toast("welcome!")
                 setJwt(r);
                 navigate(TaskListRoute)
-            })}>Click to log in as a test user
-        </button>
+            })
+        }}>
+            <div className="form-control w-full">
+                <label className="label">
+                    <span className="label-text">Email</span>
+                </label>
+                <input
+                    type="email"
+                    className="input input-bordered w-full"
+                    value={registerForm.email}
+                    onChange={(e) => setRegisterForm({...registerForm, email: e.target.value})}
+                    required
+                />
+            </div>
+
+            <div className="form-control w-full">
+                <label className="label">
+                    <span className="label-text">Password</span>
+                </label>
+                <input
+                    type="password"
+                    className="input input-bordered w-full"
+                    value={registerForm.password}
+                    onChange={(e) => setRegisterForm({...registerForm, password: e.target.value})}
+                    required
+                />
+                
+            </div>
+            <div className="form-control w-full">
+                <label className="label">
+                    <span className="label-text">Repeat Password</span>
+                </label>
+                <input
+                    type="password"
+                    className="input input-bordered w-full"
+                    value={passwordRepeat}
+                    onChange={(e) => setPasswordRepeat(e.target.value)}
+                    required
+                />
+
+            <button type="submit" className="btn btn-primary w-full">Register</button>
+            </div>
+            
+        </form>
+        
+        
+        <div>
+            <h1>test login button below:</h1>
+            <button className="btn btn-primary" onClick={() =>
+                authClient.login(new AuthRequestDto({email: "test@user.dk", password: "abc"})).then(r => {
+                    toast("welcome!")
+                    setJwt(r);
+                    navigate(TaskListRoute)
+                })}>Click to log in as a test user (this is just for local development, ill change later)
+            </button>
+
+        </div>
     </div>)
 
 
